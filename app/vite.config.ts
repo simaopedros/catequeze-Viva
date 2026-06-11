7b2a67439aaaa05008a28f3c8e59a77664fa82ab
Simao Pedro da Silva <simaopedros@gmail.com>
2026-06-11 20:14:55 -0300

    feat: adicionar Google Tag Manager (GTM-MTGNTJG6) via plugin Vite

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,23 @@
 import { wasp } from "wasp/client/vite"
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, type Plugin } from "vite";
+
+function gtmPlugin(): Plugin {
+  const gtmId = "GTM-MTGNTJG6";
+  return {
+    name: "inject-gtm",
+    transformIndexHtml(html) {
+      const headScript = `<!-- Google Tag Manager -->\n<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${gtmId}');</script>\n<!-- End Google Tag Manager -->`;
+      const noscriptTag = `\n<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`;
+      return html
+        .replace("</head>", `  ${headScript}\n  </head>`)
+        .replace("<body>", `<body>${noscriptTag}`);
+    },
+  };
+}
 
 export default defineConfig({
-  plugins: [wasp(), tailwindcss()],
+  plugins: [wasp(), tailwindcss(), gtmPlugin()],
   server: {
     open: true,
     allowedHosts: [
